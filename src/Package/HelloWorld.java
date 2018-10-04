7b9ffc6cf5e9c71ddaccaa01cb9c1c175d1c4220
Alex Klimov <meineq@gmail.com>
2018-10-05 00:00:01 +0300

    Read XML file

--- src/Package/HelloWorld.java
+++ src/Package/HelloWorld.java
@@ -1,7 +1,23 @@
 package Package;
+import java.io.*;
+import java.util.Arrays;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hellow, World!");
+        try(FileReader reader = new FileReader("./files/Example.xml"))
+        {
+            char[] buf = new char[256];
+            int c;
+            while((c = reader.read(buf))>0){
+
+                if(c < 256){
+                    buf = Arrays.copyOf(buf, c);
+                }
+                System.out.print(buf);
+            }
+        }
+        catch(IOException ex){
+            System.out.println(ex.getMessage());
+        }
     }
 }
