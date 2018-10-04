8e867e79a54f9008cf0fdfc5a508b084ec209455
Alex Klimov <meineq@gmail.com>
2018-10-04 23:54:10 +0300

    add xml file

--- src/Package/HelloWorld.java
+++ src/Package/HelloWorld.java
@@ -2,6 +2,6 @@ package Package;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.print("Hellow, World!");
+        System.out.println("Hellow, World!");
     }
 }
